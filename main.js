0bf5174f1e747fc3e227a4a2363954f208535cb2
M-B-K <mohammadbaherk@gmail.com>
2022-08-08 13:49:45 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// header
+let fixednav = document.querySelector(".header");
+window.addEventListener("scroll",()=>{
+    window.scrollY >100? fixednav.classList.add("active"): fixednav.classList.remove("active");
+})
+
+// api
+let ahadithcontnieer = document.querySelector(".ahadithcontnieer");
+let next = document.querySelector(".bottun .next");
+let prev = document.querySelector(".bottun .prev");
+let number = document.querySelector(".bottun .number");
+let hadithindex = 0;
+hadithchanger();
+function hadithchanger(){
+    // https://www.hadithapi.com/docs/hadiths
+    const apiUrl= "https://www.hadithapi.com/api/hadiths?apiKey=$2y$10$NnJbDjV2YFeiaii4ORQteezEUzRtLYcXrqedlLgpdhR1s9FKhu";
+    fetch(apiUrl).then(response => response.json()).then(data => {
+        let hadiths = data.hadiths.data;
+        // console.log(hadiths);
+        gethadith();
+        next.addEventListener('click',()=>{
+            hadithindex == 24 ? hadithindex = 0 : hadithindex++;
+            gethadith();
+        })
+        prev.addEventListener('click',()=>{
+            hadithindex == 0 ? hadithindex = 24 : hadithindex--;
+            gethadith();
+        })
+        function gethadith(){
+            ahadithcontnieer.innerText = hadiths[hadithindex].hadithArabic;
+            number.innerText = `25 / ${hadithindex + 1}`
+        }
+    })
+}
+// explore btn
+let exploreBtn = document.querySelector(".title .btn");
+let ahadithsection = document.querySelector(".ahadith");
+exploreBtn.addEventListener('click',()=>{
+ahadithsection.scrollIntoView({
+    behavior: "smooth"
+})
+})
